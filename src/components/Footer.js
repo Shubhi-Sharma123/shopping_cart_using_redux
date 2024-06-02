43f92a9929999d08a074a80f1ebed1b5b655adfe
Shubhi-Sharma123 <shubizz95@gmail.com>
2024-06-02 11:16:58 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer id="footer">
+        <div className="container">
+          <p>© 2024 India <img src="/images/india flag.png" alt="india" style={{height:20,width:20, marginRight:10}}/>IN</p>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
